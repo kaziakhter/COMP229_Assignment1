fb7d2d7af21584c436dd066de8397c9edcf7e008
kakhter <kakhter@my.centennialcollege.ca>
2023-09-27 20:43:31 -0400

    change path

--- routes/index.js
+++ routes/index.js
@@ -6,29 +6,31 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home'});
 });
 
-/* GET home page. */
+/*GET home page. */
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home'});
-});
+}); 
 
 /* GET About Us page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About'});
+  res.render('about', { title: 'About Me'});
 });
 
 /* GET Products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products'});
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects'});
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services'});
+  res.render('services', { title: 'Services'});
 });
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact'});
+  res.render('contact', { title: 'Contact'});
 });
+
+
 
 module.exports = router;
