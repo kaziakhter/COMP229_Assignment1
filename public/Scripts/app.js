67a67cacc2e9a66ed386fceaf50abe7a0f5dcfa3
kakhter <kakhter@my.centennialcollege.ca>
2023-09-27 20:44:07 -0400

    add submitForm event

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -9,3 +9,26 @@
     window.addEventListener("load", Start);
 
 })();
+
+function submitForm(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    // Capture form information 
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const phone = document.getElementById('phone').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+  
+    // For demonstration, log the captured information
+    console.log('Captured Form Information:');
+    console.log('First Name:', firstName);
+    console.log('Last Name:', lastName);
+    console.log('Phone:', phone);
+    console.log('Email:', email);
+    console.log('Message:', message);
+  
+    // Redirect to the Home Page
+    window.location.href = "/home"; 
+  }
